fd6a7f5634c488a5c904efa2e2b42c7111b96a3c
Vasiliy Startsev <Vasiliystartsev@gmail.com>
2022-06-19 23:27:50 +0300

    traffic light

--- 2.28.2/ViewController.swift
+++ 2.28.2/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var changeButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 100
+        redLight.alpha = 0.3
+
+        yellowLight.layer.cornerRadius = 100
+        yellowLight.alpha = 0.3
+
+        greenLight.layer.cornerRadius = 100
+        greenLight.alpha = 0.3
+
+        changeButton.layer.cornerRadius = 10
     }
 
+    @IBAction func changeButtonTapped() {
+        changeButton.setTitle("NEXT", for: .normal)
+        redLight.alpha = 1.0
 
+    }
 }
-
